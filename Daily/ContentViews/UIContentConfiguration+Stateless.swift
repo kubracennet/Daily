7aa0c43879a41bbc432afa533a19df1eacc91f2d
kubracennet <kubracennetyavasoglu@gmail.com>
2023-11-27 23:55:21 +0300

    Added reminder, time and date setting

---
+++ Daily/ContentViews/UIContentConfiguration+Stateless.swift
@@ -0,0 +1,14 @@
+//
+//  UIContentConfiguration+Stateless.swift
+//  Daily
+//
+//  Created by Kübra Cennet Yavaşoğlu on 22.11.2023.
+//
+
+import UIKit
+
+extension UIContentConfiguration {
+    func updated(for state: UIConfigurationState) -> Self {
+        return self
+    }
+}
